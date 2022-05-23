06a8e3f35c8e8b28d779dab6379eb2ac4916a65b
pavelnaut <pavelmbogdanov@gmail.com>
2022-05-23 15:09:10 +0300

    Publish let_go.py

---
+++ let_go.py
@@ -0,0 +1,28 @@
+if 'love' is False:
+    the = 1, not True,
+    try:
+        _=not object(),
+        def end(_no_):
+            """more"""
+    except:
+        def eat(*_and):
+            yield 'control.'
+        for go in'tent':
+            def late(**to): float()
+            while True:
+                heart = '<3'
+                break
+            feels = like = a = hex,
+            ete=r'n',all, set in {"stone"}, complex
+            unable = 2
+            del ete
+            'n'or quit()
+            let = 'it'
+            pass
+            through = 'you',
+            continue
+    cause = no = 'one'
+else:
+    will = b' your', 'ex',
+    but = any([1])
+can, be = 'your', next
